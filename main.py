3a2039574bc5f1b8229f2d0f41273dd3eaeb453d
Skimatoshi <skimatoshi@gmail.com>
2024-06-25 07:34:40 +0800

    Deleted everything

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('This is feature!')
+import pygame
+
+#initialize pygame, setting up clock/FPS
+pygame.init()
+clock = pygame.time.Clock()
+FPS = 60
+
+#define and create the window
+WIDTH, HEIGHT = 1000, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#main loop
+run = True
+while run:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+    pygame.display.flip()
